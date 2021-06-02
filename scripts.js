09fd9eb619aee9844464fc0db7401938ed6d8a50
Asier Garper <asiergarper@gmail.com>
2021-06-02 17:08:59 +0200

    Creation of the first model of LocalStorage with the Favourite List

--- scripts.js
+++ scripts.js
@@ -3,10 +3,10 @@
 document.querySelector("#searchImput").addEventListener("click", function (e) {
     e.preventDefault(); //Esto es para evitar que la pagina recarge por defecto
     let pickedArtist = (document.querySelector("#artistField").value).replace(" ", "_"); //Metemos el replace, para sustituir espacios por barras bajas
-    console.log(pickedArtist);
+    // console.log(pickedArtist);
     axios.get(`https://api.deezer.com/search/artist?q=${pickedArtist}`)
     .then(function(response1){
-        console.log(response1)
+        // console.log(response1)
         let artistData = response1.data.data
         let artist = {artistName :artistData[0].name, artistPicture : artistData[0].picture_big, artistNbAlbum : artistData[0].nb_album, artistNbFan :artistData[0].nb_fan, songList:[] }
         //De momento, he decido mostrar unicamente el primer resultado (posicion [0] en el array de artistData), a futuro dejare selecionar entre los distintos artistas encontrados con el nombre de artista introducido.)
@@ -19,21 +19,21 @@ document.querySelector("#searchImput").addEventListener("click", function (e) {
         //Una vez conseguimos el artista principal que busca el usuario (artistData[0].name), realizamos una nueva llamada a la api, pidiendo su top canciones:
         axios.get(`https://api.deezer.com/search/track?q=${artistData[0].name}`)
         .then(function(response2){
-            console.log(response2);
+            // console.log(response2);
             let trackData = response2.data.data
             trackData.forEach(trackElement => {
                 if (trackElement.artist.id == artistData[0].id){
                     //Si el id del artista de response1 coincide con el id del artista del track recibido de response2, muestro las canciones. Esto es para evitar poner canciones de otros artistas que contengan el nombre introducido. 
                     let song = {songPicture : trackElement.album.cover_medium, songTitle : trackElement.title_short, songAlbum : trackElement.album.title, songPreview : trackElement.preview}
                     
-                    console.log("Cumpliendose el if, el foreach del array de trackData da lo siguiente:")
-                    console.log(trackElement)
+                    // console.log("Cumpliendose el if, el foreach del array de trackData da lo siguiente:")
+                    // console.log(trackElement)
 
                     //-------------------Ahora voy a llamar a MusicXMatch
                     axios.get(`http://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist.artistName}}&page_size=10&page=1&s_track_rating=desc&apikey=9a02741f06fec2008254115a0846b094`)
                     .then(function(response3){
-                        console.log("Esto da el response3")
-                        console.log(response3)
+                        // console.log("Esto da el response3")
+                        // console.log(response3)
                         let songsMXM = response3.data.message.body.track_list
                         songsMXM.forEach(songsMXMElement => {
                         if (songsMXMElement.track.track_name == song.songTitle){
@@ -41,11 +41,47 @@ document.querySelector("#searchImput").addEventListener("click", function (e) {
                             console.log(songsMXMElement.track.track_name)
                             axios.get(`http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=9a02741f06fec2008254115a0846b094&track_id=${songsMXMElement.track.track_id}`)
                             .then(function(response4){
-                                console.log("Esto da response4:")
-                                console.log(response4)
+                                // console.log("Esto da response4:")
+                                // console.log(response4)
                                 song.songLyrics = response4.data.message.body.lyrics.lyrics_body; //Y aqui, metemos en nuestra lista de songs, los lyrics de cada una.
                                 //Debido a la cantidad de peticiones a API mediante Axios, y por diferentes tiempos de carga, la unica forma de cargar la letra de las canciones es mediante un funcion, llamandola cuando todas las peticiones API se hayan realizado y el cuerpo de la cancion (Titulo, album, letra, y sonidos) esten guardados en songList.
                                paintInHTMLyricsSong(song);
+                               document.querySelectorAll(".heartIcon").forEach(function(favButton){
+                                favButton.addEventListener("click", function(){
+                                    let songInfoClassPosition = this.parentNode.parentNode;
+                                    console.log("Aqui esta el node")
+                                    console.log(this.parentNode.parentNode)
+                                    let imagenCancion = songInfoClassPosition.querySelector("img").src;
+                                    console.log(songInfoClassPosition.querySelector("img").src)
+                                    //Aqui recoger los datos en el objeto favSongObject
+                                    let favSongObject = {imagen : imagenCancion}
+                                    if(!localStorage.getItem("favList")){
+                                        favSongObject = JSON.stringify([favSongObject]);
+                                        localStorage.setItem("favList", favSongObject)
+                                    }
+                                    else{
+                                        let localStorageFavSongObject = localStorage.getItem("favList")
+                                        localStorageFavSongObject = JSON.parse(localStorageFavSongObject)
+                                        let position = -1;
+                                        localStorageFavSongObject.forEach((element, index) => {
+                                            if(element.imagen == favSongObject.imagen){
+                                                position = index;
+                                            }
+                                        });
+                                        if(position==-1){
+                                            //No ha encontrado la cancion, asi que la aniade
+                                            localStorageFavSongObject.push(favSongObject)
+                                            localStorageFavSongObject = JSON.stringify(localStorageFavSongObject);
+                                            localStorage.setItem("favList", localStorageFavSongObject)
+                                        }
+                                        else{
+                                            localStorageFavSongObject.splice(position, 1)
+                                            localStorageFavSongObject = JSON.stringify(localStorageFavSongObject);
+                                            localStorage.setItem("favList", localStorageFavSongObject)
+                                        }
+                                    }
+                                })
+                               })
                             })
                         }
                         else{
@@ -77,82 +113,57 @@ document.querySelector("#searchImput").addEventListener("click", function (e) {
     
 
     function paintInHTMLyricsSong(song){
-                document.querySelector(".songInfo").innerHTML += `<img src="${song["songPicture"]}" alt="imagenDemo">
+                document.querySelector(".songInfo").innerHTML += `<div>
+                <img src="${song["songPicture"]}" alt="imagenDemo">
                                     <h3 class="title">${song["songTitle"]}</h3>
                                     <h4 class="album"><i>${song["songAlbum"]}</i></h4>
                                     <p class="lyrics">${song.songLyrics}</p>
+                                    <div class="favIcon">
+                                    <i class="fas fa-heart heartIcon" style="color: grey;"'></i>
+                                    </div>
                                     <div class="previewBottomDiv">
                                     <p>Song preview:</p>
                                     <audio controls>
                                         <source src="${song["songPreview"]}" type="audio/mpeg">
                                         Your browser does not support the audio element.
                                     </audio>
-                                    </div>`                
+                                    </div>
+                                    </div>`;      
             
     }
 
 
 
-//----------------------------Esto es antes de utilizar DEEZER como Api principal----------------
-// axios.get(
-    //          `http://api.musixmatch.com/ws/1.1/track.search?q_artist=${pickedArtist}&page_size=10&page=1&s_track_rating=desc&apikey=9a02741f06fec2008254115a0846b094`
-    //     )
-        
-    //     .then(function (response) {
-    //         console.log(response);
-    //         let data = response.data.message.body.track_list;
-    //         data.forEach(element => {
-    //             console.log("El siguiente chivato es el nombre de la cancion, y funciona:")
-    //             console.log(element.track.track_name)
-    //             console.log(element);
-    //             document.querySelector(".songResult").innerHTML += `
-                
-    //              `;
-    //              //Ahora, vamos a coger el track_id de esta peticion, para realizar otra peticion con el y conseguir la letra de la cancion:
-    //              axios.get(`http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=9a02741f06fec2008254115a0846b094&track_id=${element.track.track_id}`)
-    //              .then(function(response2){
-    //                  console.log("Esto da la response2:");
-    //                  console.log(response2);
-    //                  let lyricsData = response2.data.message.body.lyrics
-    //                  console.log(lyricsData)
-    //                  document.querySelector(".songResult").innerHTML += `<div class="songInfo${element.track.track_name}">
-    //                                                                         <h3 class="title">${element.track.track_name}</h3>
-    //                                                                         <h4 class="album"><i>${element.track.album_name}</i></h4>
-    //                                                                         <p class="lyrics">${lyricsData.lyrics_body}</p>
-    //                                                                             <div class="previewBottomDiv">
+//----------------------------Javascript para implementar funcionalidad a elementos HTML--------------------------------------------------------
 
-    //                                                                             </div>
-    //                                                                         </div>`
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
 
-    //              })
-    //              axios.get(`https://api.deezer.com/search/track?q=${pickedArtist}`)
-    //              .then(function(response3){
-    //                  console.log("Esto da la response3:")
-    //                  console.log(response3)
-    //                  let deezerArtistData = response3.data.data;
-    //                  deezerArtistData.forEach(deezerElement => {
-    //                      console.log(document.querySelector(`.songInfo${deezerElement.title}`));
-    //                      console.log(document.querySelector(`.songInfo${element.track.track_name}`));
-    //                      if (deezerElement.title==element.track.track_name){
-    //                          //aqui, comparar titulo de deezer con el titulo de musicXMatch, si coincide, sacar su preview (su extracto de mp3 para el boton)
-    //                          document.querySelector(`.songInfo${element.track.track_name}`).innerHTML += `<img src="${deezerElement.album.cover_medium}" alt="imagenDemo">
-    //                                                                             <p>Song preview:</p>
-    //                                                                             <audio controls>
-    //                                                                                 <source src="${deezerElement.preview}" type="audio/mpeg">
-    //                                                                                 Your browser does not support the audio element.
-    //                                                                             </audio>`                                                                                    
-    //                      }
-    //                      //Si en la busqueda realizada, el titulo de la cancion de la Api de Deezer no coincide con el titulo de la canciond e la Api de MusicXMatch, se introduce un pequenio parrafo que lo explique.
-    //                     //  else{
-    //                     //      document.querySelector(".songInfo").innerHTML += '<p>Preview no disponible</p>';
-    //                     //  }
-    //                  });
-    //              })
-    //         });
 
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     })
-// }
-// )}
+  //-------------------------------Boton Favoritos------------------------------Hay que crear dos funciones, usando como comprobador, el estado del color del corazon. Si esta rojo y se clicka, fucion aniadir a favoritos, que anada a la lista de favoritos todo. Lo mismo cuando este en gris, funcion quitar de favoritos.
+
+function favouritesFunction(song){
+    console.log(song)
+    // document.querySelector(".heartIcon").addEventListener("click", function(){
+        if (document.querySelector(".heartIcon").style.color == "grey"){
+            document.querySelector(".heartIcon").style.color = "red"
+            //Aqui, meter todo lo referente a guardar datos a favoritos
+        } else if (document.querySelector(".heartIcon").style.color == "red"){
+                document.querySelector(".heartIcon").style.color = "grey"
+            //Aqui, meter todo lo referente a guardar datos a favoritos
+    
+        }
+    // })
+}
+
+
+
